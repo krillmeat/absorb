e9febedfcd9de503891208245759821c80ced2f2
Ross <conover.ross@gmail.com>
2021-03-12 23:28:47 -0600

    add stuph

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,9 +1,34 @@
+var debug;
+
 window.onload = function(){
     init();
 }
 
+/** 
+ * INITIALIZE
+ * ----------------------------------------------------------------------
+ * The inital Function that runs after the constructor
+ * ----------------------------------------------------------------------
+ */
 function init(){
     console.log("%cInitializing...","color:#999");
 
-    // setHeroToWindowHeight();
+    setupDebug();
+
+    setHeroHeight();
+    
+}
+
+function setHeroHeight(){
+  document.getElementById("hero").style.height = document.querySelector(".big-text-wrap").offsetHeight + "px";
+}
+
+/**
+ * SETUP DEBUG
+ * ----------------------------------------------------------------------
+ * Creates an instance of the Debug Class
+ * ----------------------------------------------------------------------
+ */
+function setupDebug(){
+  debug = new DEBUGTOOL(false,null,[8,8,16]);
 }
